ef80cd1b49fb880d3373bf0fdae4a160a9c7cd1c
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-24 13:08:44 -0700

    add submit behavior

--- team-builder/src/components/Form.tsx
+++ team-builder/src/components/Form.tsx
@@ -5,19 +5,31 @@ interface FormProps {
   addToTeam: (team: string, teamMember: TeamMember) => void
 }
 
-function Form(props: FormProps): ReactElement {
+function Form({ addToTeam }: FormProps): ReactElement {
+  const [team, setTeam] = useState<string>('')
   const [teamMember, setTeamMember] = useState<TeamMember>({
     name: '',
     email: '',
     role: '',
   })
 
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    addToTeam(team, teamMember)
+  }
+
+  const onChangeTeam = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setTeam(event.target.value)
+  }
+
   const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setTeamMember({ ...teamMember, [event.target.id]: event.target.value })
   }
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
+      <label htmlFor="team">Team:</label>
+      <input type="text" id="team" value={team} onChange={onChangeTeam} />
       <label htmlFor="name">Name:</label>
       <input
         type="text"
@@ -39,6 +51,7 @@ function Form(props: FormProps): ReactElement {
         value={teamMember.role}
         onChange={onChange}
       />
+      <button>Submit</button>
     </form>
   )
 }
