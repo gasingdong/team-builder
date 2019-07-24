837f426139f47b8aa950173ba0537a5a0b9e18ba
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-24 12:35:30 -0700

    add data binding from input to team member info

--- team-builder/src/components/Form.tsx
+++ team-builder/src/components/Form.tsx
@@ -1,14 +1,45 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 
 function Form(): ReactElement {
+  interface TeamMember {
+    name: string
+    email: string
+    role: string
+  }
+
+  const [teamMember, setTeamMember] = useState<TeamMember>({
+    name: '',
+    email: '',
+    role: '',
+  })
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setTeamMember({ ...teamMember, [event.target.id]: event.target.value })
+  }
+
   return (
     <form>
       <label htmlFor="name">Name:</label>
-      <input type="text" id="name" />
+      <input
+        type="text"
+        id="name"
+        value={teamMember.name}
+        onChange={onChange}
+      />
       <label htmlFor="email">E-Mail:</label>
-      <input type="text" id="email" />
+      <input
+        type="text"
+        id="email"
+        value={teamMember.email}
+        onChange={onChange}
+      />
       <label htmlFor="role">Role:</label>
-      <input type="text" id="role" />
+      <input
+        type="text"
+        id="role"
+        value={teamMember.role}
+        onChange={onChange}
+      />
     </form>
   )
 }
